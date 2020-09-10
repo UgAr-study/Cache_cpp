862487967b6cb8fbaec8b7dab3b941bc6d6f85c7
UgAr-study <uglovskij.au@gmail.com>
2020-09-10 18:44:38 +0300

    without tests generator

--- cache2_0.cpp
+++ cache2_0.cpp
@@ -19,36 +19,37 @@ private:
     } main, in, out;
 
 public:
-    explicit Cache (size_t m_s, size_t i_s, size_t o_s) : main(m_s),
-                                                 in(i_s),
-                                                 out(o_s) {};
+    explicit Cache (size_t size_main, size_t size_in, size_t size_out) :
+        main(size_main),
+        in(size_in),
+        out(size_out) {};
 
-    unsigned int LookUpHits (std::vector<T> pages);
-    bool Is_in_queue (T page, un_map hash) { return (hash.find(page) != hash.end()); };
+    size_t LookUpHits (std::vector<T> pages);
 
-    void MovePage_FromFirst_ToSecondContainer (T page, Container& container1, Container& container2);
+    static void MovePage_FromFirst_ToSecondContainer (T page, Container& container1,
+                                                              Container& container2);
     void AddNewPage(T page);
 };
 
-int main () {
-    int page, m_size, in_size, out_size;
-    scanf ("%d", &m_size);
-    scanf ("%d", &in_size);
-    scanf ("%d", &out_size);
+std::vector<int> GetIntPage (const char *file_name);
 
+int main (int argc, char* argv[]) {
     std::vector<int> pages;
-    while ((scanf("%d", &page)) == 1)
-        pages.push_back(page);
 
-    Cache<int> c = Cache<int> (m_size, in_size, out_size);
+    if (argc == 5) {
+        Cache<int> cache = Cache<int> ((size_t)strtol(argv[1], nullptr, 0),
+                                       (size_t)strtol(argv[2], nullptr, 0),
+                                       (size_t)strtol(argv[3], nullptr, 0));
+        pages = GetIntPage(argv[4]);
+        printf ("result is %zu\n", cache.LookUpHits(pages));
+    }
 
-    printf ("\nresult is %d\n", c.LookUpHits(pages));
     return 0;
 }
 
 template<typename T>
-unsigned int Cache<T>::LookUpHits(std::vector<T> pages) {
-    unsigned int hits = 0;
+size_t Cache<T>::LookUpHits(std::vector<T> pages) {
+    size_t hits = 0;
     auto page_iter = pages.begin();
 
     for (; page_iter != pages.end(); ++page_iter) {
@@ -77,15 +78,14 @@ unsigned int Cache<T>::LookUpHits(std::vector<T> pages) {
     return hits;
 }
 
-
 template<typename T>
 void Cache<T>::MovePage_FromFirst_ToSecondContainer(T page,
                                                     Cache::Container &container1,
                                                     Cache::Container &container2) {
 
     if ((&container1 != &container2) && (container2.list.size() == container2.size)) {
-        container2.hash_table.erase(*(--container2.list.end()));
-        container2.list.erase((--container2.list.end()));
+        container2.hash_table.erase(container2.list.back());
+        container2.list.pop_back();
     }
 
     auto iter = container1.hash_table[page];
@@ -98,14 +98,18 @@ void Cache<T>::MovePage_FromFirst_ToSecondContainer(T page,
 template<typename T>
 void Cache<T>::AddNewPage(T page) {
     if (in.list.size() == in.size)
-        MovePage_FromFirst_ToSecondContainer(*(--in.list.end()), in, out);
+        MovePage_FromFirst_ToSecondContainer(in.list.back(), in, out);
 
     in.list.push_front(page);
     in.hash_table[page] = in.list.begin();
 }
 
-//template<typename T>
-//T getPage(std::string basicString) {
-//    return nullptr;
-//}
-
+std::vector<int> GetIntPage (const char *file_name) {
+    FILE *file = fopen(file_name, "r");
+    fseek(file, 0, SEEK_SET);
+    int page = 0;
+    std::vector<int> pages;
+    while (fscanf(file, "%d", &page) == 1)
+        pages.push_back(page);
+    return pages;
+}
